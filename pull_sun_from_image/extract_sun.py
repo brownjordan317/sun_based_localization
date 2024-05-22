ca636d9c11e7d4c4db3ce091e4b86be8954e4034
CowbungaMk1 <166857125+cadengraf@users.noreply.github.com>
2024-05-22 09:58:48 -0500

    change
    nothing

--- pull_sun_from_image/extract_sun.py
+++ pull_sun_from_image/extract_sun.py
@@ -2,8 +2,10 @@ import sys
 from PIL import Image, ImageDraw
 import math 
 import os
+import cv2
+import numpy as np
 
-def extract_white_and_darker_pixels(input_image, output_image):
+def extract_white_and_darker_pixels(input_image):
     with Image.open(input_image) as im:
         # Ensure input image is RGB
         if im.mode != "RGB":
@@ -21,13 +23,24 @@ def extract_white_and_darker_pixels(input_image, output_image):
                 # Get pixel color
                 pixel = im.getpixel((x, y))
                 # Check if pixel is white or the next three darker shades
-                for i in range(235, 252):
+                for i in range(235, 255):
                     if pixel[0] >= i and pixel[1] >= i and pixel[2] >= i:
                         # Set pixel to white in output image
                         white_image.putpixel((x, y), pixel)
                         # Store coordinates of white pixel
                         white_points.append((x, y))
                         break
+
+
+        file_name = os.path.basename(input_image)
+        base_name = os.path.splitext(file_name)[0]
+        # Create the directory if it doesn't exist
+        directory = "sun_pulled_images"
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        # Save the image to the directory
+        output_image = f"{directory}/{base_name}_extracted.jpg"
 
         # Save the output image
         white_image.save(output_image)
@@ -61,10 +74,40 @@ def extract_white_and_darker_pixels(input_image, output_image):
             center_x = (max_density_segment[0] * segment_size) + (segment_size // 2)
             center_y = (max_density_segment[1] * segment_size) + (segment_size // 2)
             
+            # output_image_cv2 = cv2.imread(output_image)
+
+            # white_image = cv2.cvtColor(output_image_cv2, cv2.COLOR_BGR2GRAY) 
+    
+            # # Blur using 3 * 3 kernel. 
+            # gray_blurred = cv2.blur(white_image, (3, 3)) 
+            
+            # # Apply Hough transform on the blurred image. 
+            # detected_circles = cv2.HoughCircles(gray_blurred,  
+            #                 cv2.HOUGH_GRADIENT, 1.2, 20, param1 = 50, 
+            #             param2 = 30, minRadius = 1, maxRadius = 200) 
+            
+            # # Draw circles that are detected. 
+            # if detected_circles is not None: 
+            
+            #     # Convert the circle parameters a, b and r to integers. 
+            #     detected_circles = np.uint16(np.around(detected_circles)) 
+            
+            #     for pt in detected_circles[0, :]: 
+            #         a, b, r = pt[0], pt[1], pt[2] 
+            
+            #         # Draw the circumference of the circle. 
+            #         cv2.circle(output_image_cv2, (a, b), r, (0, 255, 0), 2) 
+            
+            #         # Draw a small circle (of radius 1) to show the center. 
+            #         cv2.circle(output_image_cv2, (a, b), 1, (0, 0, 255), 3) 
+            #         cv2.imshow("Detected Circle", output_image_cv2) 
+            #         cv2.waitKey(0) 
+            #         cv2.destroyAllWindows()
+                        
             # Add a red point at the center
             red_point_image = white_image.copy()
             draw = ImageDraw.Draw(red_point_image)
-            radius = segment_size = math.floor(width / height * (math.pow(10, exponent) / 3) )  # Adjust the radius to control the size of the red point
+            radius = segment_size = math.floor(width / height * (math.pow(10, exponent) / 7) )  # Adjust the radius to control the size of the red point
             draw.ellipse([(center_x - radius, center_y - radius),
                             (center_x + radius, center_y + radius)],
                             fill=(255, 0, 0))
@@ -84,16 +127,17 @@ def extract_white_and_darker_pixels(input_image, output_image):
                 os.makedirs(directory)
 
             # Save the image to the directory
-            print(f"{directory}/{base_name}.png")
             new_img.save(f"{directory}/{base_name}_overlay.png", "PNG")
+            
+            
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <input_image> <output_image>")
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <input_image>")
         sys.exit(1)
     
     input_image = sys.argv[1]
-    output_image = sys.argv[2]
 
-    extract_white_and_darker_pixels(input_image, output_image)
+
+    extract_white_and_darker_pixels(input_image)
